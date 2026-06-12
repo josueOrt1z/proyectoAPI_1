81361e34bf22bf6bbbb1be8445b8be88bc87e851
Josue <josueortizalv@gmail.com>
2026-06-12 11:41:59 -0500

    se alade el metodo renderizarRepositorios y se implmentan em buscarUsuario( )

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
-import { buscarUsuario, mostrar } from "./metodos.js";
+import { buscarUsuario} from "./metodos.js";
 
 const inputuUsername = document.querySelector("#username");
 const botonBuscar = document.querySelector("#buscar");
 const loading = document.querySelector("#loading");
 export const resultado = document.querySelector("#resultado");
-
+const repos = document.querySelector("#repos");
 
 
 botonBuscar.addEventListener("click", ()=>{
     const usuario = inputuUsername.value;
-    buscarUsuario(loading, resultado, usuario);
-   
+    buscarUsuario(loading, resultado, repos, usuario);
+    
 })
--- metodos.js
+++ metodos.js
@@ -1,7 +1,8 @@
 
-export function buscarUsuario (loading, resultado, usuario){
+export function buscarUsuario (loading, resultado, repos, usuario){
+
     loading.textContent = "Cargando"
-    let usuarioABuscar = fetch(`https://api.github.com/users/${usuario}`)
+    fetch(`https://api.github.com/users/${usuario}`)
     .then(result => {
         if (result.ok != true){
             throw new Error ("Usuario no encontrado")
@@ -9,25 +10,43 @@ export function buscarUsuario (loading, resultado, usuario){
         else {
             return result.json();//las promesas en {} deben llevar return
         }})
-        .then(usuario => mostrar(null, resultado, usuario))//se llama adentro para evitar que se cargue el null
+        .then(usuario => {mostrar(null, resultado, repos, usuario)//se llama adentro para evitar que se cargue el null
+            renderizarRepositorios(repos, usuario)//no se pone en el literal pues arroja undefined por ser un metodo vacio
+        })
         .catch(error => mostrar(error, resultado, null))
         .finally(()=>loading.textContent = "")
     }
 
-export function mostrar (error, contenedor, usuario){
+
+function mostrar (error, contenedor, repos, usuario){
     if (usuario){
         const perfil =
            `<img src="${usuario.avatar_url}"></img>
             <h1> ${usuario.login}</h1>       
             <p>Seguidores: ${usuario.followers} </p>
             <p>Repositorios publicos: ${usuario.public_repos}</p>`
+            
         contenedor.innerHTML = perfil
         }
         else {
             contenedor.textContent = error.message
-        }
+        }           
+}
+
+
+function renderizarRepositorios(contenedor, usuario){
+    fetch(usuario.repos_url)
+    .then(repos => repos.json())
+    .then(arrayRepos =>{
+        console.log(arrayRepos)
+        const repos = arrayRepos.map((rep)=>{
             
-            
+            return `<ul><li>${rep.name}</li></ul>`
+        })
+        const strRepos = repos.join("");
+        contenedor.innerHTML = strRepos;
+    })
+    
 }
 
 function mostrarElemento(elemento){
